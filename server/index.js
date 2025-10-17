fec6a1620ee3ca6f91e5e2b42a8c8d661080cd39
PawleN-D <sirpawle@gmail.com>
2025-10-18 00:49:03 +0200

    initial commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { createHandler } from "graphql-http/lib/use/express";
+import { buildSchema } from "graphql";
+ 
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+ 
+const root = {
+  hello() {
+    return "Hello world!";
+  },
+};
+ 
+const app = express();
+ 
+app.all(
+  "/graphql",
+  createHandler({
+    schema,
+    rootValue: root,
+  })
+);
+ 
+app.listen(4000, () => {
+  console.log("Running a GraphQL API server at http://localhost:4000/graphql");
+});
